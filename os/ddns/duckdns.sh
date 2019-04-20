eac9a06b7695be5cb1a636718866dd122243b429
Mike Loosbrock <mike.loosbrock@code42.com>
2019-04-20 09:24:37 -0500

    initial commit

---
+++ os/ddns/duckdns.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+domains="$(hostname)"
+token="NOT-IN-GIT"
+
+curl_exit=$(echo url="https://www.duckdns.org/update?domains=${domains}&token=${token}" | /usr/bin/curl -skK -)
+
+if [ ${curl_exit} == "OK" ]; then
+  logger -p daemon.info "duckdns update ok for {${domains}}.duckdns.org"
+else
+  logger -p daemon.err "duckdns update failed for {${domains}}.duckdns.org"
+fi
