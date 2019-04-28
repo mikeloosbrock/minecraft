be6e3704ef6bc2056e8061773d2ab4d5af3e3740
Mike Loosbrock <mike.loosbrock@code42.com>
2019-04-28 13:26:29 -0500

    updated spigot start script

--- spigot/start.sh
+++ spigot/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 version=$1
-cd ${version}
-java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-${version}.jar --world-dir worlds"
+cd /var/minecraft/spigot/${version}
+java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-${version}.jar --world-dir "worlds"
